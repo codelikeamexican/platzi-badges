16e66bddd007b52182b59d16c1c04242a7a094f2
codelikeamexican <codelikeamexican@gmail.com>
2020-04-28 19:25:58 -0700

    Creacion y diseño de componentes, Reto Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,32 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React from 'react';
 import './styles/Home.css';
-import platziconfLogoImage from '../images/platziconf-logo.svg';
-import astronautsImage from '../images/astronauts.svg';
+import {Link} from 'react-router-dom';
+import confLogo from '../images/platziconf-logo.svg';
+import astrounauts from '../images/astronauts.svg';
 
-export default class Home extends Component {
-  render() {
-    return (
+
+class Home extends React.Component{
+  render(){
+    return(
       <div className="Home">
         <div className="container">
-          <div className="row">
-            <div className="Home__col col-12 col-md-4">
-              <img
-                src={platziconfLogoImage}
-                alt="Platzi Conf Logo"
-                className="img-fluid mb-2"
-              />
-
-              <h1>Badge Management System</h1>
-              <Link className="btn btn-primary" to="/badges">
-                Start
+        <div className="row">
+         <div className="Home__col col-12 col-md-4">
+              <img src={confLogo}  />
+             <h1> Platzi Badges</h1>
+              <h5 className="font-weight-light"> La conferencia anual de platzi </h5>
+              <Link className="btn btn-primary"
+              to="/badges"> 
+              Get Start
               </Link>
-            </div>
-
-            <div className="Home__col d-none d-md-block col-md-8">
-              <img
-                src={astronautsImage}
-                alt="Astronauts"
-                className="img-fluid p-4"
-              />
-            </div>
           </div>
+        <div className="Home__col col-md-8">
+        <img src={astrounauts}/>
+        </div>
         </div>
       </div>
+    </div>     
     );
   }
 }
+export default Home;
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { render } from 'react-dom';
+import './styles/NotFound.css';
 
+import {Link} from 'react-router-dom';
 function NotFound() {
-  return <h1>404: Not Found</h1>;
+ 
+    return(
+      <div className="NotFound">
+        <div className="container">
+
+          <div className="row">
+            <div className="NotFound__col col-4">
+              <h1>Error : 404</h1>
+              <h3> Página no encontrada</h3>
+              <Link className="btn btn-primary" 
+              to="/"
+              > 
+              Regresar al inicio
+              </Link>
+            </div>
+
+            <div className="NotFound__col col-8">
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    );
+  
 }
 
 export default NotFound;
